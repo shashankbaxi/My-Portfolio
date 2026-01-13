bc9c4669e8a23903b6b0767ca5cac8e41593ea10
Shashank Baxi <141709845+shashankbaxi@users.noreply.github.com>
2026-01-13 17:11:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(), // ✅ must go BEFORE react()
-    react(),
-  ],
+  plugins: [react()],
 });
