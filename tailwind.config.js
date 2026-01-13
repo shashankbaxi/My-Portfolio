b285142a46a89a8bfe3eba38e9f0055049d77746
Shashank Baxi <141709845+shashankbaxi@users.noreply.github.com>
2026-01-13 17:11:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: [
-    "text-blue-500",
-    "bg-black",
-    "bg-white",
-    "flex",
-    "grid",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
+
