84378a9d536700440b779007a651e826b9a4a8ca
Shashank Baxi <141709845+shashankbaxi@users.noreply.github.com>
2026-03-21 20:26:26 +0530

    Update skills.jsx

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const skillGroups = [
   {
     title: "Languages",
-    skills: ["HTML", "CSS", "JavaScript", "Java(Basic)"],
+    skills: ["HTML", "CSS", "JavaScript",],
   },
   {
     title: "Frameworks",
